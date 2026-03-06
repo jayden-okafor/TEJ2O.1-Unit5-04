4c1394d7033fc25c2c1d49558e440c1a56c948f6
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-03-06 03:32:14 +0000

    changed py code

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,5 +5,34 @@ This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+import neopixel
 
-display.scroll("Hello, World!")
+neopixelStrip = neopixel.NeoPixel(pin16, 4)
+
+neopixelStrip.clear()
+
+display.show(Image.HAPPY)
+
+while True:
+    
+    if button_a.is_pressed():
+        display.clear()
+        display.show(Image.YES)
+
+        neopixelStrip[2] = (0, 255, 0)
+        neopixelStrip.show()
+        sleep(1000)
+
+        neopixelStrip[2] = (0, 0, 0)
+        neopixelStrip[1] = (255, 255, 0)
+        neopixelStrip.show()
+        sleep(500)
+
+        neopixelStrip[1] = (0, 0, 0)
+        neopixelStrip[0] = (255, 0, 0)
+        neopixelStrip.show()
+        sleep(1000)
+
+        neopixelStrip.clear()
+        display.clear()
+        display.show(Image.HAPPY)
